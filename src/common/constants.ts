7b89c831ea6eab36dcf5c537717e5ac58d5cb3b7
Thi Nguyen <anhthi.ieig@gmail.com>
2023-10-14 16:43:07 +0700

    Some basic components and constatns

---
+++ src/common/constants.ts
@@ -0,0 +1,17 @@
+export enum MfePort {
+  BASE_APP = 3000,
+  CRYPTO_INFO = 3001,
+  HOLDINGS = 3002,
+  ORDER_MGT = 3003,
+  LATEST_STORIES = 3004,
+  PLACED_TRADE = 3005,
+}
+
+export enum MfeName {
+  BASE_APP = "baseApp",
+  CRYPTO_INFO = "cryptoInfo",
+  HOLDINGS = "holdings",
+  ORDER_MGT = "orderMgt",
+  LATEST_STORIES = "latestStories",
+  PLACED_TRADE = "placedTrade",
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
+export * from "./common/constants";
+export * from "./common/types";
 export * from "./components";
+export * from "./hooks";
+export * from "./styles";
