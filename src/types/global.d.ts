be7fbfe8e33166415113dfedc082d81162557c6a
Thi Nguyen <anhthi.ieig@gmail.com>
2023-10-26 21:24:25 +0700

    Add global declaration

---
+++ src/types/global.d.ts
@@ -0,0 +1,6 @@
+declare const __webpack_init_sharing__: (shareScope: string) => Promise<void>;
+declare const __webpack_share_scopes__: { default: string };
+
+interface Window {
+  [key: string]: any;
+}
